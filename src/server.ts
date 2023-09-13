a6162cb13ac8d2a0d2e7582256121184c1c1b883
gabriel-vitebo <vitebo@outlook.com>
2023-09-13 18:42:06 -0300

    create route to upload video

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import { fastify } from "fastify";
 import { getAllPromptsRoute } from "./routes/getAllPrompts";
+import { uploadVideoRoutes } from "./routes/uploadVideo";
 
 const app = fastify()
 
 app.register(getAllPromptsRoute)
+app.register(uploadVideoRoutes)
 
 app.listen({
   port: 3333,
