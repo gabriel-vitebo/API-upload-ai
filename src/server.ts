ae72353b68a1079dd000d207ca67e3d01ac1ddd0
gabriel-vitebo <vitebo@outlook.com>
2023-09-13 20:37:17 -0300

    creating a route to generate AI summary

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,20 @@
 import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors";
 import { getAllPromptsRoute } from "./routes/getAllPrompts";
 import { uploadVideoRoutes } from "./routes/uploadVideo";
 import { createTranscriptionRoute } from "./routes/createTranscription";
+import { generateAiCompletionRoute } from "./routes/generateAiCompletion";
 
 const app = fastify()
+
+app.register(fastifyCors, {
+  origin: '*',
+})
 
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoutes)
 app.register(createTranscriptionRoute)
+app.register(generateAiCompletionRoute)
 
 app.listen({
   port: 3333,
