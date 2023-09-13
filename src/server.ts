1e1e78f9bf3ebb481ef9ff7988cd007f32d72f5d
gabriel-vitebo <vitebo@outlook.com>
2023-09-13 20:08:48 -0300

    creating a route to generate the transcript

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import { fastify } from "fastify";
 import { getAllPromptsRoute } from "./routes/getAllPrompts";
 import { uploadVideoRoutes } from "./routes/uploadVideo";
+import { createTranscriptionRoute } from "./routes/createTranscription";
 
 const app = fastify()
 
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoutes)
+app.register(createTranscriptionRoute)
 
 app.listen({
   port: 3333,
